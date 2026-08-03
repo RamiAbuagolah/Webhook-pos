f9797816d83c923b8d2e07ef37538dfe2ce04ce7
RamiAbuagolah <86856062+RamiAbuagolah@users.noreply.github.com>
2026-08-03 09:46:58 +0300

    Add configuration namespace to Functions host

--- OrderPulse.Webhooks.Functions/Program.cs
+++ OrderPulse.Webhooks.Functions/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
